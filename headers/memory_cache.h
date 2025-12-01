eb0a27dced673f35f2be06c52379b79bff5da4b3
Pramod Patil <pramodpatil5366@gmail.com>
2025-12-01 10:25:30 +0000

    memory cache functions are added

--- headers/memory_cache.h
+++ headers/memory_cache.h
@@ -1,6 +1,43 @@
 #ifndef MEMORY_CACHE_H
 #define MEMORY_CACHE_H
 
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<semaphore.h>
 
+#define SHARED_MEMORY_KEY 0x1234
+#define CACHE_SIZE 128
+#define VALUE_SIZE 256
+
+typedef struct {
+    char value[VALUE_SIZE];
+    int key;
+    int valid;   //0 for empty, 1 for used
+    int lru_counter;  //for LRU
+} Cache_Entry;
+
+typedef struct {
+    Cache_Entry entries[CACHE_SIZE];
+    sem_t mutex;  //semaphore for synchronization
+    int initialized; //avoid re-initialization by each process
+}Shared_Cache;
+
+//shared memory functions
+int cache_shm_create();
+Shared_Cache* cache_shm_attach(int shmid);
+int cache_shm_detach(Shared_Cache* cache);
+int cache_shm_destroy(int shmid);
+
+//cache operations
+void cache_init(Shared_Cache* cache);
+int cache_put(Shared_Cache* cache, int key, const char* value);
+int cache_get(Shared_Cache* cache, int key);
+int cache_evict(Shared_Cache* cache); //evicting based on LRU
+
+
+//synchronization functions
+void cache_lock(Shared_Cache *cache);
+void cache_unlock(Shared_Cache *cache);
 
 #endif
